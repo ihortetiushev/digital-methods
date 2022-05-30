f08fb02fab0c4764f6c54e9ed64cc22e350b8820
ihortetiushev <ihor.tetiushev@gmail.com>
2022-05-30 10:10:24 +0300

    added Element to keep the index

--- lb3/lb3.cpp
+++ lb3/lb3.cpp
@@ -5,25 +5,48 @@
 #include <string>
 using namespace std;
 
-void printMatrix(double** matrix, int rows, int columns)
+struct Element
+{
+    int index = -1;
+    double coefficient = nan("");
+    double x = nan("");
+    Element() 
+    {
+        int index = -1;
+        double coefficient = nan("");
+        double x = nan("");
+    }
+    Element(int index, double coefficient) 
+    {
+        this->index = index;
+        this->coefficient = coefficient;
+    }
+    Element(double coefficient)
+    {//this is free member
+         this->coefficient = coefficient;
+         this->x = 1.0;
+    }
+};
+void printMatrix(Element** matrix, int rows, int columns)
 {
     for (int i = 0; i < rows; i++) 
     {
         for (int k = 0; k < columns; k++) 
         {
-            cout << setw(10) << left << matrix[i][k];
+            cout << setw(8) << left << setprecision(3) << matrix[i][k].coefficient;
         }
         cout << endl;
     }
 }
+
 struct MainElement {
     int p=0;
     int q=0;
     int size = 0;
     double value=0;
-    double* row;
+    Element* row;
 };
-MainElement findMainElement(double** matrix, int rows, int columns)
+MainElement findMainElement(Element** matrix, int rows, int columns)
 {
     double max = -DBL_MAX;
     MainElement mainElement;
@@ -31,12 +54,12 @@ MainElement findMainElement(double** matrix, int rows, int columns)
     {
         for (int k = 0; k < columns-1; k++)
         {
-            if (fabs(matrix[i][k]) > max)
+            if (fabs(matrix[i][k].coefficient) > max)
             {
-                max = fabs(matrix[i][k]);
+                max = fabs(matrix[i][k].coefficient);
                 mainElement.p = i;
                 mainElement.q = k;
-                mainElement.value = matrix[i][k];
+                mainElement.value = matrix[i][k].coefficient;
                 mainElement.row = matrix[i];
             }
         }
@@ -44,31 +67,31 @@ MainElement findMainElement(double** matrix, int rows, int columns)
     mainElement.size = columns;
     return mainElement;
 }
-double* calculateM(double** a, int rows, int columns, MainElement main)
+double* calculateM(Element** a, int rows, int columns, MainElement main)
 {
     double* m = new double[rows];
     for (int i = 0; i < rows; i++) 
     {
         if (i != main.p)
         {
-            m[i] = -1 * a[i][main.q] / main.value;
+            m[i] = -1 * a[i][main.q].coefficient / main.value;
         }
     }
     return m;
 }
-double** createMatrix(int rows, int columns)
+Element** createMatrix(int rows, int columns)
 {
-    double** matrix;
-    matrix = new double* [rows];
+    Element** matrix;
+    matrix = new Element * [rows];
     for (int i = 0; i < columns; i++)
     {
-        matrix[i] = new double[columns];
+        matrix[i] = new Element[columns];
     }
     return matrix;
 }
-double** decreaseMatrix(double** a, int rows, int columns, MainElement main)
+Element** decreaseMatrix(Element** a, int rows, int columns, MainElement main)
 {
-    double** result = createMatrix(rows - 1, columns - 1);
+    Element** result = createMatrix(rows - 1, columns - 1);
     int newRow = 0;
     for (int i = 0; i < rows; i++)
     {
@@ -88,50 +111,65 @@ double** decreaseMatrix(double** a, int rows, int columns, MainElement main)
     }
     return result;
 }
-double** multiplyM(double** a, int rows, int columns, MainElement main, double* m)
+Element** multiplyM(Element** a, int rows, int columns, MainElement main, double* m)
 {
-    double** interimResult = createMatrix(rows, columns);
+    Element** interimResult = createMatrix(rows, columns);
     for (int i = 0; i < rows; i++)
     {
         if (i != main.p)
         {
             for (int j = 0; j < columns; j++)
             {
-                interimResult[i][j] = a[i][j] + a[main.p][j] * m[i];
+                interimResult[i][j].coefficient = a[i][j].coefficient + a[main.p][j].coefficient * m[i];
+                interimResult[i][j].index = a[i][j].index;
+                interimResult[i][j].x = a[i][j].x;
             }
         }
     }
-    double** result = decreaseMatrix(interimResult, rows, columns, main);
+    Element** result = decreaseMatrix(interimResult, rows, columns, main);
     delete[] * interimResult;
     return result;
 }
-double calculateRoot(MainElement elem, double results[], int resultIndex, int toFindIndex) 
+void calculateRoot(MainElement & elem)
 {
     double sum = 0;
+    //getting summ of all known members
     for (int i = 0; i < elem.size - 1; i++) 
     {
-        if (!isnan(results[i])) 
+        if (!isnan(elem.row[i].x))
         {
-            sum = sum + elem.row[i] * results[i];
+            sum = sum + elem.row[i].x * elem.row[i].coefficient;
         }
     }
-    double res = (elem.row[elem.size - 1] - sum) / elem.row[toFindIndex];
-    results[resultIndex] = res;
-    return res;
+    //find first coefficient with unknown x
+    Element toFind;
+    int resultIndex = -1;
+    for (int i = 0; i < elem.size - 1; i++) 
+    {
+        if (isnan(elem.row[i].x)) 
+        {
+            toFind = elem.row[i];
+            resultIndex = i;
+            break;
+        }
+    }
+    double freeMember = elem.row[elem.size - 1].coefficient;
+    double res = (freeMember - sum) / toFind.coefficient;
+    elem.row[resultIndex].x = res;
 }
 void printMainElement(MainElement elem)
 {
     cout << "main element" << endl;
     for (int i = 0; i < elem.size; i++) 
     {
-        cout << setw(8) << elem.row[i];
+        cout << setw(8) << setprecision(3) <<elem.row[i].coefficient;
     }
     cout << endl;
 }
-vector<MainElement> getAllMainElements(double** matrix, int rows, int cols)
+vector<MainElement> getAllMainElements(Element** matrix, int rows, int cols)
 {
     vector<MainElement> mainElements;
-    double** matr = matrix;
+    Element** matr = matrix;
     int currentRow = rows;
     int currentColumn = cols;
     while (currentRow > 0)
@@ -141,7 +179,7 @@ vector<MainElement> getAllMainElements(double** matrix, int rows, int cols)
         mainElements.push_back(mainElement);
         cout << endl;
         double* m = calculateM(matr, rows, cols, mainElement);
-        double** newMatrix = multiplyM(matr, rows, cols, mainElement, m);
+        Element** newMatrix = multiplyM(matr, rows, cols, mainElement, m);
         currentRow--;
         currentColumn--;
         printMatrix(newMatrix, currentRow, currentColumn);
@@ -149,27 +187,57 @@ vector<MainElement> getAllMainElements(double** matrix, int rows, int cols)
     }
     return mainElements;
 }
-int *main()
+void populateResolved(MainElement & toPopulate, Element root) 
+{
+    Element* singleRow = toPopulate.row;
+    for (int i = 0; i < toPopulate.size; i++)
+    {
+        if (root.index == singleRow[i].index) 
+        {
+            singleRow[i].x = root.x;
+            break;
+        }
+    }
+}
+void populateResolved(vector<MainElement> allmainElements, MainElement resolved) 
+{
+    for (MainElement toPopulate : allmainElements)
+    {
+        Element* results = resolved.row;
+        for (int i = 0; i < resolved.size; i++)
+        {
+            if (!isnan(results[i].x))
+            {
+                populateResolved(toPopulate, results[i]);
+            }
+        }
+    }
+}
+int main()
 {
     const int ROWS = 3;
     const int COLUMNS = 4;
-    double** initialMatrix;
+    Element** initialMatrix;
 
-    initialMatrix = new double* [ROWS];
-    initialMatrix[0] = new double[COLUMNS] { 6, 10, 12, 50 };
-    initialMatrix[1] = new double[COLUMNS] { 0, 4,  8,  24 };
-    initialMatrix[2] = new double[COLUMNS] { 2, 1,  1,  6 };
+    initialMatrix = new Element * [ROWS];
+    initialMatrix[0] = new Element[COLUMNS] { Element(0,6), Element(1,10), Element(2,12), Element(50), };
+    initialMatrix[1] = new Element[COLUMNS] { Element(0,0), Element(1,4),  Element(0,8),  Element(24) };
+    initialMatrix[2] = new Element[COLUMNS] { Element(0,2), Element(1,1),  Element(0,1),  Element(6) };
 
     printMatrix(initialMatrix, ROWS, COLUMNS);
 
     vector<MainElement> mainElements = getAllMainElements(initialMatrix, ROWS, COLUMNS);
  
-    double result[ROWS] = {nan(""),nan(""), nan("")};
-
-    double x1 = calculateRoot(mainElements.at(2), result, 1, 0);
-    double x0 = calculateRoot(mainElements.at(1), result, 0, 0);
-    double x2 = calculateRoot(mainElements.at(0), result, 2, 2);
-    cout << "x0 = " << setw(3) << x0 << endl;
-    cout << "x1 = " << setw(3) << x1 << endl;
-    cout << "x2 = " << setw(3) << x2 << endl;
+    for (int i = mainElements.size() - 1; i >= 0; i--)
+    {
+        MainElement toResolve = mainElements.at(i);
+        calculateRoot(toResolve);
+        populateResolved(mainElements, toResolve);
+    }
+    //top first  main element contains all roots
+    Element *result = mainElements.at(0).row;
+    for (int i=0; i< mainElements.size(); i++)
+    {
+       cout << "x" + to_string(result[i].index) + " = " << setw(3) << result[i].x << endl;
+    }
 }
