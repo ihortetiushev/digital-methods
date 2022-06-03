f324e7b585401dcc64f65b712ffa1175195d0651
ihortetiushev <ihor.tetiushev@gmail.com>
2022-06-03 20:15:14 +0300

    final solution

--- lb3/lb3.cpp
+++ lb3/lb3.cpp
@@ -3,7 +3,13 @@
 #include <math.h>
 #include <vector>
 #include <string>
+
 using namespace std;
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const double ACCURACY = 1e-3;
+const int MAX_ATTEMPT_COUNT = 1000;
 
 struct Element
 {
@@ -213,21 +219,20 @@ void populateResolved(vector<MainElement> allmainElements, MainElement resolved)
         }
     }
 }
-int main()
+void gaussMethodWithMaineElement() 
 {
-    const int ROWS = 3;
-    const int COLUMNS = 4;
+
     Element** initialMatrix;
 
     initialMatrix = new Element * [ROWS];
-    initialMatrix[0] = new Element[COLUMNS] { Element(0,6), Element(1,10), Element(2,12), Element(50) };
-    initialMatrix[1] = new Element[COLUMNS] { Element(0,0), Element(1,4),  Element(2,8),  Element(24) };
-    initialMatrix[2] = new Element[COLUMNS] { Element(0,2), Element(1,1),  Element(2,1),  Element(6) };
-
+    initialMatrix[0] = new Element[COLUMNS]{ Element(0,6), Element(1,10), Element(2,12), Element(50) };
+    initialMatrix[1] = new Element[COLUMNS]{ Element(0,0), Element(1,4),  Element(2,8),  Element(24) };
+    initialMatrix[2] = new Element[COLUMNS]{ Element(0,2), Element(1,1),  Element(2,1),  Element(6) };
+   
     printMatrix(initialMatrix, ROWS, COLUMNS);
 
     vector<MainElement> mainElements = getAllMainElements(initialMatrix, ROWS, COLUMNS);
- 
+
     for (int i = mainElements.size() - 1; i >= 0; i--)
     {
         MainElement toResolve = mainElements.at(i);
@@ -235,9 +240,92 @@ int main()
         populateResolved(mainElements, toResolve);
     }
     //top first  main element contains all roots
-    Element *result = mainElements.at(0).row;
+    Element* result = mainElements.at(0).row;
     for (unsigned int i = 0; i < mainElements.size(); i++)
     {
-       cout << "x" + to_string(result[i].index) + " = " << setw(3) << result[i].x << endl;
+        cout << "x" + to_string(result[i].index) + " = " << setw(3) << result[i].x << endl;
     }
+}
+double getx0(double x1, double x2) 
+{
+    return (50.0 - 10.0 * x1 - 12.0 * x2) / 6.0;
+}
+double getx1(double x2)
+{
+    return (24.0 - 8.0 * x2) / 4.0;
+}
+double getx2(double x0, double x1)
+{
+    return 6.0 - 2 * x0 - x1;
+}
+double max(double a, double b, double c)
+{
+    double max;
+    if (a > b && a > c) 
+    {
+        max = a;
+    }
+    else if (b > c) 
+    {
+        max = b;
+    }
+    else 
+    {
+        max = c;
+    }
+    return max;
+}
+void calc(double initx0, double initx1, double initx2)
+{
+    double x0 = initx0;
+    double x1 = initx1;
+    double x2 = initx2;
+    double diff;
+    int count = 0;
+    do 
+    {
+        double nextx0 = getx0(x1, x2);
+        double nextx1 = getx1(x2);
+        double nextx2 = getx2(x0, x1);
+        double deltax0 = fabs(nextx0 - x0);
+        double deltax1 = fabs(nextx1 - x1);
+        double deltax2 = fabs(nextx2 - x2);
+        diff = max(deltax0, deltax1, deltax2);
+        x0 = nextx0;
+        x1 = nextx1;
+        x2 = nextx2;
+        count++;
+        if(count > MAX_ATTEMPT_COUNT)
+        {
+            throw "Can't calculate roots. To many attemps.";
+        }
+    }
+    while (diff > ACCURACY);
+    cout << "x0 = " << setw(3) << x0 << endl;
+    cout << "x1 = " << setw(3) << x1 << endl;
+    cout << "x2 = " << setw(3) << x2 << endl;
+}
+
+int main()
+{
+    cout << "----------------------------------------" << endl;
+    cout << "Gauss Method With Main Element Selection" << endl;
+    cout << "----------------------------------------" << endl;
+    gaussMethodWithMaineElement();
+    cout << "----------------------------------------" << endl;
+    cout << "Itherations Method" << endl;
+    cout << "----------------------------------------" << endl;
+    double x0init = 50.0 / 6.0;
+    double x1init = 24.0 / 4.0;
+    double x2init = 6.0;
+    try
+    { 
+        calc(x0init, x1init, x2init);
+    }
+    catch (char const* err)
+    {
+      cout << err << endl;
+    }
+
+    cout << "----------------------------------------" << endl;
 }
